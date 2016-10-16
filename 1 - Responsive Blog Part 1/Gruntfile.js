e42ae5ae8144883c72dbdb9840b92cb246728b1d
Franco Sarachu <fsarachu@gmail.com>
2016-10-15 22:35:02 -0300

    Reformat Gruntfile.js

--- 1 - Responsive Blog Part 1/Gruntfile.js
+++ 1 - Responsive Blog Part 1/Gruntfile.js
@@ -1,12 +1,12 @@
 /*
  After you have changed the settings at "Your code goes here",
  run this with one of these options:
-  "grunt" alone creates a new, completed images directory
-  "grunt clean" removes the images directory
-  "grunt responsive_images" re-processes images without removing the old ones
-*/
+ "grunt" alone creates a new, completed images directory
+ "grunt clean" removes the images directory
+ "grunt responsive_images" re-processes images without removing the old ones
+ */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   grunt.initConfig({
     responsive_images: {
@@ -20,9 +20,9 @@ module.exports = function(grunt) {
         },
 
         /*
-        You don't need to change this part if you don't change
-        the directory structure.
-        */
+         You don't need to change this part if you don't change
+         the directory structure.
+         */
         files: [{
           expand: true,
           src: ['*.{gif,jpg,png}'],
